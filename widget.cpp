428c3ec58f6b0d47b1eea2b1e23a7ce128b5622e
Victor Potasyev <vpotasyev@gmail.com>
2018-09-21 21:44:08 +0300

    Finished form

--- widget.cpp
+++ widget.cpp
@@ -13,11 +13,33 @@ Widget::Widget(QWidget *parent)
     qlblConfirm->setBuddy(qledConfirm);
     qledConfirm->setEchoMode(QLineEdit::Password);
 
+    QLabel* qlblWebSite  = new QLabel("&Web Site");
+    QLineEdit* qledWebSite  = new QLineEdit;
+    qlblWebSite->setBuddy(qledWebSite);
+
+    QLabel* qlblResult = new QLabel("&Result");
+    QLineEdit* qledResult  = new QLineEdit;
+    qlblResult->setBuddy(qledResult);
+
+    QPushButton* qbtnGenerate = new QPushButton("Generate");
+    QPushButton* qbtnGenCopy = new QPushButton("Generate and copy");
+
+    QHBoxLayout* buttonLayout = new QHBoxLayout;
+    buttonLayout->addWidget(qbtnGenerate);
+    buttonLayout->addWidget(qbtnGenCopy);
+
     QVBoxLayout* layout = new QVBoxLayout;
     layout->addWidget(qlblMaster);
     layout->addWidget(qledMaster);
     layout->addWidget(qlblConfirm);
     layout->addWidget(qledConfirm);
+    layout->addWidget(qlblWebSite);
+    layout->addWidget(qledWebSite);
+    layout->addWidget(qlblResult);
+    layout->addWidget(qledResult);
+    layout->addLayout(buttonLayout);
+    layout->addStretch();
+
     this->setLayout(layout);
 }
 
